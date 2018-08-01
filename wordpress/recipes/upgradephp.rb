438561e6078e60d83d45188e0727cde2c6c1c357
Debabrata Karfa <debabrata@rippling.com>
2018-08-01 15:47:18 +0530

    Update on command

--- wordpress/recipes/upgradephp.rb
+++ wordpress/recipes/upgradephp.rb
@@ -1,7 +1,19 @@
 execute "install-python-software-properties" do
   command "apt-get install python-software-properties"
+end
+
+execute "add-repository" do
   command "add-apt-repository ppa:ondrej/php"
+end
+
+execute "update-repository-source" do
   command "apt update"
+end
+
+execute "install-php7.2" do
   command "apt install -y php7.2"
+end
+
+execute "install-php-associate" do
   command "apt install php7.2-curl php7.2-gd php7.2-json php7.2-mbstring php7.2-mcrypt"
 end
