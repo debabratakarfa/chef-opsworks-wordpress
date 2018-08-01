ec08adb0ad99651ac92448e038f5393e567cda8a
Debabrata Karfa <debabrata@rippling.com>
2018-08-01 15:39:46 +0530

    Make compatible with Chef 11.10

--- wordpress/recipes/upgradephp.rb
+++ wordpress/recipes/upgradephp.rb
@@ -1,53 +1,7 @@
 execute "install-python-software-properties" do
   command "apt-get install python-software-properties"
-end
-
-execute "add-php-source"
   command "add-apt-repository ppa:ondrej/php"
-end
-
-apt_update 'update'
-
-apt_package 'php7.2' do
-  action :install
-end
-
-apt_package 'php-pear' do
-  action :install
-end
-
-apt_package 'php7.2-curl' do
-  action :install
-end
-
-apt_package 'php7.2-dev' do
-  action :install
-end
-
-apt_package 'php7.2-gd' do
-  action :install
-end
-
-apt_package 'php7.2-json' do
-  action :install
-end
-
-apt_package 'php7.2-mbstring' do
-  action :install
-end
-
-apt_package 'php7.2-mcrypt' do
-  action :install
-end
-
-apt_package 'php7.2-zip' do
-  action :install
-end
-
-apt_package 'php7.2-mysql' do
-  action :install
-end
-
-apt_package 'php7.2-xml' do
-  action :install
+  command "apt update"
+  command "apt install -y php7.2"
+  command "apt install php7.2-curl php7.2-gd php7.2-json php7.2-mbstring php7.2-mcrypt"
 end
